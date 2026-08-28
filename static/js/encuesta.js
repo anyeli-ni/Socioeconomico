e12e73edc7043cdc65ef4b76a794b649d05f6e5e
anyeli-ni <137965024+anyeli-ni@users.noreply.github.com>
2026-08-28 11:03:57 -0500

    2 BASE

--- static/js/encuesta.js
+++ static/js/encuesta.js
@@ -1,3 +1,41 @@
+let seccionActual = 0;
+
+function mostrarSeccion(numero) {
+
+    const secciones = document.querySelectorAll(".seccion");
+
+    secciones.forEach(seccion => {
+        seccion.classList.remove("activa");
+    });
+
+    secciones[numero].classList.add("activa");
+
+}
+
+
 function siguienteSeccion() {
-    alert("La navegacion funcionara hasta aquí.");
+
+    const secciones = document.querySelectorAll(".seccion");
+
+    if (seccionActual < secciones.length - 1) {
+
+        seccionActual++;
+
+        mostrarSeccion(seccionActual);
+
+    }
+
+}
+
+
+function anteriorSeccion() {
+
+    if (seccionActual > 0) {
+
+        seccionActual--;
+
+        mostrarSeccion(seccionActual);
+
+    }
+
 }
